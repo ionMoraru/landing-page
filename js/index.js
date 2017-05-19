33c7ebb16d22cf5429cd745b2a662adb8a889293
Moraru Ion <morarou.y@gmail.com>
2017-05-19 17:16:35 +0200

    slider and smooth scroll done

--- js/index.js
+++ js/index.js
@@ -24,3 +24,23 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 
 }
+
+const windowWidth = window.innerWidth;
+if(windowWidth<800) {
+  let slideIndex = 0;
+  showSlides();
+
+  function showSlides() {
+    let i;
+    const textSlide = document.getElementsByClassName('text-slide');
+    for (i = 0; i < textSlide.length; i++) {
+      textSlide[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > textSlide.length) {slideIndex = 1}
+    textSlide[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 7000)
+  }
+}else {
+  console.log('erro');
+}
---
+++ js/smoothscroll.js
@@ -0,0 +1,6 @@
+$('.smoothScroll').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+    }, 1000);
+    return false;
+});
