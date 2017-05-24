c2b1e97c02948a56f5adff932d6520aa0a8c3ded
Moraru Ion <morarou.y@gmail.com>
2017-05-24 13:12:44 +0200

    slide test

--- js/index.js
+++ js/index.js
@@ -12,13 +12,13 @@ function showSlides(n) {
   const slides = document.getElementsByClassName('mySlides');
   const textSlide = document.getElementsByClassName('text-slide');
   const dots = document.getElementsByClassName('dot');
-
   if (n > slides.length) {slideIndex = 1}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < textSlide.length; i++) {
     textSlide[i].style.display = "none";
+    textSlide[i].className = textSlide[i].className.replace(" text-slide-animation", "");
   }
   for (i = 0; i <dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
